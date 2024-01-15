f14f939310fba79de25391d3c33983b710fb2c1a
teckenrod <treyeck2@gmail.com>
2024-01-15 10:45:24 -0600

    Rearranged files to abstract view

--- H4X0R/Views/DetailView.swift
+++ H4X0R/Views/DetailView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct DetailView: View {
+    let url: String?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WebView(urlString: url)
     }
 }
 
 #Preview {
-    DetailView()
+    DetailView(url: "https://www.google.com")
 }
