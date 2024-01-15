6bb10e88dc2a14613e15bf1845a5bfb99a6fd372
teckenrod <treyeck2@gmail.com>
2024-01-15 10:02:23 -0600

    basic view

--- H4X0R/ContentView.swift
+++ H4X0R/ContentView.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            
+            .navigationTitle("H4X0R News")
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola")
+]
