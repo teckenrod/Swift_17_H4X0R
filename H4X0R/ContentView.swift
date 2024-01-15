17ddfedb73cc3bbe0c92a1e1a9cb56db91ced287
teckenrod <treyeck2@gmail.com>
2024-01-15 09:52:49 -0600

    Initial Commit

---
+++ H4X0R/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  H4X0R
+//
+//  Created by Trey Eckenrod on 1/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Trey Eckenrod on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
